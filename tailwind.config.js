1af6ce5036a58ca9fcae7b0a9e3b3e327b02dfff
rcesonis <rimvydas.cesonis@gmail.com>
2024-11-10 17:12:11 +0100

    Finished announcement bar

--- tailwind.config.js
+++ tailwind.config.js
@@ -24,7 +24,7 @@ module.exports = {
       },
       fontSize: {
         xs: '0.75rem',
-        sm: '0.875rem',
+        sm: '1.4rem',
         base: '1rem',
         lg: '1.125rem',
         xl: '1.25rem',
