3ecaf8dee5f63cced7ce02f96abf8d1f7bee3603
rcesonis <rimvydas.cesonis@gmail.com>
2024-11-10 15:10:11 +0100

    Theme setu up

--- tailwind.config.js
+++ tailwind.config.js
@@ -20,6 +20,13 @@ module.exports = {
       fontFamily: {
         heading: 'var(--font-heading-family)',
       },
+      fontSize: {
+        xs: '0.75rem',
+        sm: '0.875rem',
+        base: '1rem',
+        lg: '1.125rem',
+        xl: '1.25rem',
+      },
     },
   },
   plugins: [],
